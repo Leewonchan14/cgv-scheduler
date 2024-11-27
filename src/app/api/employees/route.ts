975a908952cb299b90e35c810f64da634d136f6c
이원찬 <twoone14@tukorea.ac.kr>
2024-11-27 14:51:23 +0900

    직원 id들로 직원 가져오는 라우트 핸들러 구현

---
+++ src/app/api/employees/route.ts
@@ -0,0 +1,19 @@
+import { IEmployee } from '@/entity/employee.entity';
+import { ERole } from '@/entity/enums/ERole';
+import { employeeService } from '@/feature/employee/employee.service';
+import { appDataSource } from '@/share/libs/typerom/data-source';
+import _ from 'lodash';
+
+export async function GET() {
+  const [employees, _total] = await employeeService(
+    await appDataSource(),
+  ).findAll(0, 30, '');
+
+  const response = {
+    data: employees
+      .map((emp) => _.omit(emp, ['password']))
+      .filter((emp) => emp.role !== ERole.ADMIN) as IEmployee[],
+  };
+
+  return Response.json(response);
+}
